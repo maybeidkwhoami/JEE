9d3dd86d3ed0e0e62313455817c776a3d5c39862
Mohamed <124465334+maybeidkwhoami@users.noreply.github.com>
2024-03-10 15:02:19 +0000

    Delete TP2GESTION directory

--- TP2GESTION/src/main/java/com/example/tp2gestion/dao/repositories/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tp2gestion.dao.repositories;
-
-import com.example.tp2gestion.dao.entities.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository <Professor, Integer> {
-}
--- TP2GESTION/src/main/java/com/example/tp2gestion/dao/repositories/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tp2gestion.dao.repositories;
-
-import com.example.tp2gestion.dao.entities.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, Integer> {
-}
